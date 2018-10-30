fcf1ccce2fc5f7fdffeebee14cfdf1029cd55315
connor mcdonough <connor.mcdonough28@gmail.com>
2018-10-30 09:12:13 -0600

    react hw

--- src/Account/index.js
+++ src/Account/index.js
@@ -43,7 +43,8 @@ class Account extends Component {
   // set a local variable to the new balance based off of the original balance + amount
   const newBalance = this.state.balance - amount;
   if(newBalance<0){
-    console.log('no funds');
+    alert("not enough funds")
+  
     return
   }
   // set the balance to the newBalance using the setState method (necessary)
